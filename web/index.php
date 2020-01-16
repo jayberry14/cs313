2f20b988379522fbededd224ad0ccf9bacaa83f7
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-16 14:33:43 -0700

    Added Homepage and Directory Assignments (W02)

--- web/index.php
+++ web/index.php
@@ -1,5 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Josh's Assignment Directory</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
+    <script src='directory.js'></script>
+</head>
+<body>
+    <h1>Table of Contents</h1>
+    <div>
+        <a href="hello.html">Assignment 1</a>
+        <br>
+        <a href="Team/index.html">Team Activity 1</a>
+        <br>
+        <a href="Personal/w2/homepage.html">Something About Me</a>
+    </div>
+</body>
+</html>
+
 <?php
-
-  phpinfo();
-
+  echo "Hello World!";
+  //phpinfo();
+​
 ?>
