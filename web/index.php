0a65e242245b39143ac182c81921c2084ef35c0c
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-17 18:22:41 -0700

    Updated Homepage with new JS and PHP

--- web/index.php
+++ web/index.php
@@ -6,9 +6,9 @@
     <title>Josh's Assignment Directory</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <!--JQERRY--><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!--?--><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!--BOOTSTRAP--><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
     <script>
 			// prevents refresh from submitting form and clears out unneeded variables
@@ -23,11 +23,14 @@
     <?php session_start(); ?>
         <h1>The Homepage for Joshua Mayberry</h1>
     </header>
-    <div class="row buttonBox btn-group" id="navBar">
-        <div class="col buttonBox button">
-            <button onclick="navigate('assignmentsBut')" class=" row btn btn-primary" type="button" id="assignmentsBut">Assignments</button>
-            <button onclick="navigate('aboutBut')" class=" row btn btn-primary" type="button" id="aboutBut">About Me</button>
+    <div class="container-fluid">
+        <div class="row buttonBox btn-group" id="navBar">
+            <div class="col buttonBox button">
+                <button onclick="navigate('assignmentsBut')" class=" row btn btn-primary" type="button" id="assignmentsBut">Assignments</button>
+                <button onclick="navigate('aboutBut')" class=" row btn btn-primary" type="button" id="aboutBut">About Me</button>
+            </div>
         </div>
+        <img src="self-portrait.jpg" alt="Me">
     </div>
 </body>
 </html>
