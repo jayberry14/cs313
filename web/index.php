f98a21c4606a4375f4cb59c62e990456b62d4176
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-17 19:22:39 -0700

    Updated Homepage with new JS and PHP

--- web/index.php
+++ web/index.php
@@ -18,29 +18,10 @@
     </script>
 </head>
 <body>
-
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Me</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">WHO</a></li>
-                    <li><a href="assignments.php">Assignments</a></li>
-                    <li><a href="#">WHERE</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- <div class="container-fluid bg-1 text-center">
+    <?php include 'header.php';?>
+    <div class="container-fluid bg-1 text-center">
         <img src="self-portrait.jpg" alt="Me" class="img-circle img-1">
-    </div> -->
+    </div>
 </body>
 </html>
 
