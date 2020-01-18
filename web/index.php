d9dc15e74d741fe96006ff38e4f337b8131707bb
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-17 18:11:14 -0700

    Updated Homepage with new JS and PHP

--- web/index.php
+++ web/index.php
@@ -23,12 +23,10 @@
     <?php session_start(); ?>
         <h1>The Homepage for Joshua Mayberry</h1>
     </header>
-    <div class="row buttonBox" id="navBar">
+    <div class="row buttonBox btn-group" id="navBar">
         <div class="col buttonBox">
-            <button onclick="navigate('assignmentsBut')" class="button" type="button" id="assignmentsBut">Assignments</button>
-        </div>
-        <div class="col buttonBox">
-            <button onclick="navigate('aboutBut')" class="button" type="button" id="aboutBut">About Me</button>
+            <button onclick="navigate('assignmentsBut')" class="btn btn-primary" type="button" id="assignmentsBut">Assignments</button>
+            <button onclick="navigate('aboutBut')" class="btn btn-primary" type="button" id="aboutBut">About Me</button>
         </div>
     </div>
 </body>
