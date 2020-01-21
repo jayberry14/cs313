0933b1bd3468ffffde691bfe1f5df73cdd3e37d1
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-21 16:18:02 -0700

    Week 3 Team Activity added

--- web/Team/ta02POST.php
+++ web/Team/ta02POST.php
@@ -3,17 +3,17 @@
     $name = htmlspecialchars($_POST["nameForPHP"]);
     $email = htmlspecialchars($_POST["emailForPHP"]);
     $major = htmlspecialchars($_POST["majorForPHP"]);
-    $visitedContinents = htmlspecialchars($_POST["visitedContinents[]"]);
+    $visitedContinents = array(htmlspecialchars($_POST["visitedContinents[]"]));
     $comments = htmlspecialchars($_POST["comments"]);
     
-    echo "Name: $name <br>";
-    echo "Email: $email <br>";
-    echo "Major: $major <br>";
+    echo "Name: $name <br><br>";
+    echo "Email: $email <br><br>";
+    echo "Major: $major <br><br>";
     echo "Visited Countries: <br>";
     foreach($_POST["visitedContinents"] as $value)
     {
         echo $value;
         echo "<br>";
     }
-    echo "Comments: $comments";
+    echo "<br>Comments: $comments";
 ?>
