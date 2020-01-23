26bf8dfd51f40e309be26f3724cc85b5f6658a38
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-23 14:28:52 -0700

    Updating Prove 3

---
+++ web/Personal/w3/checkout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Josh's Assignment Directory</title>
+    <link rel="icon" type="image/ico" href="https://cdn3.iconfinder.com/data/icons/round-icons-vol-2/512/j-512.png">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
+    <script>
+			// prevents refresh from submitting form and clears out unneeded variables
+            // Thank you Brother Birch for this section of JS!
+            if ( window.history.replaceState ) {
+                window.history.replaceState( null, null, window.location.href );
+            }
+    </script>
+    </head>
+    <body>
+    <?php include '../../header.php'?>
+    <?php session_start(); ?>
+        
+    </body>
+</html>
