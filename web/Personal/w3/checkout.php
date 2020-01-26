b6e2934626bd4bd4e9934fe539bd41a920b60282
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-25 18:17:20 -0700

    Updating Prove 3

--- web/Personal/w3/checkout.php
+++ web/Personal/w3/checkout.php
@@ -1,6 +1,6 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html class="no-js">
+<html lang="en">
     <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
--- web/Personal/w3/confirm.php
+++ web/Personal/w3/confirm.php
@@ -1,6 +1,6 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html class="no-js">
+<html lang="en">
     <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -11,13 +11,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
-    <script>
-			// prevents refresh from submitting form and clears out unneeded variables
-            // Thank you Brother Birch for this section of JS!
-            if ( window.history.replaceState ) {
-                window.history.replaceState( null, null, window.location.href );
-            }
-    </script>
+    
     </head>
     <body>
 
