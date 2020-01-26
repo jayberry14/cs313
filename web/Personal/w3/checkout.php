b877f96da578453847bde9ef9457f505f64b72d5
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-25 18:05:30 -0700

    Updating Prove 3

--- web/Personal/w3/checkout.php
+++ web/Personal/w3/checkout.php
@@ -11,15 +11,47 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
-    <script>
-			// prevents refresh from submitting form and clears out unneeded variables
-            // Thank you Brother Birch for this section of JS!
-            if ( window.history.replaceState ) {
-                window.history.replaceState( null, null, window.location.href );
-            }
-    </script>
     </head>
+    <?php
+        $_SESSION["street"];
+        $_SESSION["city"];
+        $_SESSION["state"];
+        $_SESSION["zip"];
+    ?>
+    <?php
+        if(isset($_POST["submit"])){
+            $_SESSION["street"] = htmlspecialchars($_POST["street"]);
+            $_SESSION["city"] = htmlspecialchars($_POST["city"]);
+            $_SESSION["state"] = htmlspecialchars($_POST["state"]);
+            $_SESSION["zip"] = htmlspecialchars($_POST["zip"]);
+        }
+    ?>
     <body>
-            
+        <h1>Checkout</h1>
+        <form mehtod="post">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"><label>Street: </label><input type="text" name="street" size="30"><br><br></div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"><label>City: </label><input type="text" name="city" size="30"><br><br></div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"><label>State: </label><input type="text" name="state" size="30"><br><br></div>
+                <div class="col"></div>
+            </div>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"><label>Zip: </label><input type="text" name="zip" size="30"><br><br></div>
+                <div class="col"></div>
+            </div>
+            <button type="submit" name="submit">Save Address</button>
+        </form>
+        <p class="p2"><a href="viewcart.php"><button>Back to Cart</button></a></p>
+        <p class="p1"><a href="confrim.php"><button>Place Order</button></a></p>
     </body>
 </html>
