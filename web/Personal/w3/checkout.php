fb86be403a723eff2fd35dca500dbc3270196a8f
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-25 20:56:42 -0700

    Updating Prove 3

--- web/Personal/w3/checkout.php
+++ web/Personal/w3/checkout.php
@@ -60,12 +60,10 @@
             </div>
             <button type="submit" name="submit">Save Address</button>
         </form>
+        <p><a href="viewcart.php"><button>Back to Cart</button></a></p>
+        <br>
+        <p><a href="confirm.php"><button>Place Order</button></a></p>
         </article>
     </session>
-    <br>
-    <br>
-    <p><a href="viewcart.php"><button>Back to Cart</button></a></p>
-    <br>
-    <p><a href="confirm.php"><button>Place Order</button></a></p>
     </body>
 </html>
