e5ed19ba1038d5c5e901c88aecc7faff7115d51b
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-01-28 15:19:49 -0700

    Updating landing page

--- web/Personal/w2/rideshare.php
+++ web/Personal/w2/rideshare.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div class="container-fluid bg-3"><h1>Ride Share</h1></div>
-    <div class="container-fluid bg-1" style="height:100%">
+    <div class="container-fluid bg-1" style="height:25vw">
 
     </div>
 </body>
