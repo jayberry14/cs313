6ed225669537a5ffe8b650913ece6c1972d6f8a8
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-02-11 17:57:31 -0700

    Tuesday classwork w06

--- web/Team/addScripture.php
+++ web/Team/addScripture.php
@@ -22,6 +22,7 @@
         <?php 
             echo "Before SQL prepare!<br>";
             $statement = $db->prepare('SELECT * FROM topics');
+            var_dump($statement);
             echo "In the middle!<br>";
             $statement->execute();
             echo "after execute()<br>";
