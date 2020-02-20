a9ed12bf5e6a3d3f335b8fbf1a1c33aef1aaec08
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-02-20 13:01:14 -0700

    Updating Ride Share

--- web/Personal/w2/ridesharelanding.php
+++ web/Personal/w2/ridesharelanding.php
@@ -58,9 +58,9 @@
         <div class="col-lg-6">
             <h2>Create an Account</h2>
             <?php
-                    if(isset($_GET["error"])) { ?>
-                        <p style="color:red"><?php echo "Username already taken!" ?></p>
-                <?php }
+                if(isset($_GET["error"])) { ?>
+                    <h4 style="color:red"><?php echo "Username already taken, please choose another" ?></h4>
+            <?php }
             ?>
             <form action="rideshareregister.php" method="post" style="max-width:100%">
                 <div class="form-group">
