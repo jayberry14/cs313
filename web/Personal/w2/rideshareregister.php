083bc786edc025bfc7c4de43d293b060ec19661c
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-02-20 00:57:23 -0700

    Updating Ride Share

--- web/Personal/w2/rideshareregister.php
+++ web/Personal/w2/rideshareregister.php
@@ -20,9 +20,9 @@
         $usernameCheck = $db->prepare('SELECT username FROM rider WHERE username = :username');
         $usernameCheck->bindValue(':username', $username, PDO::PARAM_STR);
         while ($row = $usernameCheck->fetch(PDO::FETCH_ASSOC)) {    // Cycle through all the different usernames in my table
-            if ($row["username"] = $username) {                     // If any of them match the user's input
+            if ($username = $row["username"]) {                     // If any of them match the user's input
                 $uName = $username;                                 // Set that input to a temp variable
-                echo "Error: Username taken already. Select a different one.";
+                ?><h4>TEST</h4><?php
             }
         }
 
