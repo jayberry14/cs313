9769dac1fa9758a3dea8c142748caf257a36a3b9
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-02-20 16:20:01 -0700

    Updating Ride Share

--- web/Team/ta07.sql
+++ web/Team/ta07.sql
@@ -3,6 +3,6 @@ DROP TABLE w07users;
 CREATE TABLE w07users
 (
     id SERIAL NOT NULL,
-    username VARCHAR(30) NOT NULL, 
-    pass_hash VARCHAR(30) NOT NULL
+    username VARCHAR(100) NOT NULL, 
+    pass_hash VARCHAR(100) NOT NULL
 );
