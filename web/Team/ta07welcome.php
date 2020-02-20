fb62993f4c2b970a1f3011efb702530e978e611f
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-02-20 16:46:04 -0700

    Updating Ride Share

--- web/Team/ta07welcome.php
+++ web/Team/ta07welcome.php
@@ -44,7 +44,7 @@
                         <input class="form-control" type="password" name="password" id="password" required>
                         <?php
                             if (isset($_GET["alphaNumError"])) { ?>
-                                <p style="color: red"><?="Password must match!"?></p>
+                                <p style="color: red"><?php echo "Password must match!"?></p>
                            <?php } ?>
                     </div>
                     <div class="form-row">
@@ -52,7 +52,7 @@
                         <input class="form-control" type="password" name="confirmPassword" id="password" required>
                         <?php
                             if (isset($_GET["confirmError"])) { ?>
-                                <p style="color: red"><?="Password must match!"?></p>
+                                <p style="color: red"><?php echo "Password must match!"?></p>
                            <?php } ?>
                     </div>
                     <br>
