4004dad293a16a81de46cbfae65f0673c72d24cd
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-03-26 11:22:39 -0600

    Ben's App

--- web/Personal/w5/BenPost.php
+++ web/Personal/w5/BenPost.php
@@ -1,12 +1,12 @@
 <?php 
     $priceX = htmlspecialchars($_POST["pricexForPHP"]);
     $priceY = htmlspecialchars($_POST["priceyForPHP"]);
+    $sumXY = $priceX + $priceY;
     $budgetCon = htmlspecialchars($_POST["budgetConstraint"]);
     $A = htmlspecialchars($_POST["inputa"]);
     $B = htmlspecialchars($_POST["inputb"]);
     
     echo "Price of X: $priceX <br><br>";
     echo "Price of Y: $priceY <br><br>";
-    echo "X + Y = $priceX + $priceY <br>";
-    
+    echo "X + Y =  $sumXY <br>";
 ?>
