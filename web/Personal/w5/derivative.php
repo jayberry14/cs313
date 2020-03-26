59ffecfa6a3e4ff23eac12968685c34580ca1097
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-03-26 11:06:09 -0600

    Ben's App

--- web/Personal/w5/derivative.php
+++ web/Personal/w5/derivative.php
@@ -7,12 +7,11 @@
         <h2>Do some derivatives :)</h2>
 
         a<i>x</i><sup>b</sup>
-        <form method="post" action="index.php">
+        <form method="post" action="">
         <input name="inputa" id="inputa" placeholder="Enter a">
         <input name="inputb" id="inputb" placeholder="Enter b">
         <input type="submit" name="submit" id="submit" value="Calculate Derivative">
         <br><br>
-
 
         <?php
 
