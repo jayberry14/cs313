38bb18c0be0c14a06ad39f4ed69b2c89d5f94096
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-04-01 21:44:22 -0600

    Ben's App

--- web/Personal/w5/Ben.php
+++ web/Personal/w5/Ben.php
@@ -6,7 +6,7 @@
         <title>Optimizing Supply</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/ico" href="https://i.stack.imgur.com/YEOj8.png">
+        <link rel="icon" type="image/ico" href="https://m.media-amazon.com/images/I/81AmfxvCLCL._SR500,500_.jpg">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
