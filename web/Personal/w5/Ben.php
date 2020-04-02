add4b7c1213c3b87a268884c159cd704cd2cfdf1
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-04-01 20:05:38 -0600

    Ben's App

--- web/Personal/w5/Ben.php
+++ web/Personal/w5/Ben.php
@@ -6,21 +6,25 @@
         <title>Super Calculator</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         <form action="BenPost.php" method="POST">
-            <div>
-                <label for="inputIncome">Income</label>
-                <input name="inputIncome" type="text" class="form-control" placeholder="Enter the Income"> <!-- I -->
-                <label for="inputGoodX">Good X's Preference</label>
-                <input name="inputGoodX" type="text" class="form-control" placeholder="Enter X's Preference"> <!-- Alpha -->
-                <label for="inputGoodY">Good Y's Preference</label>
-                <input name="inputGoodY" type="text" class="form-control" placeholder="Enter Y's Preference"> <!-- Beta -->
-                <label for="inputPriceX">Price of X</label>
-                <input name="inputPriceX" type="text" class="form-control" placeholder="Enter Price of X"> <!-- px -->
-                <label for="inputPriceY">Price of Y</label>
-                <input name="inputPriceY" type="text" class="form-control" placeholder="Enter Price of Y"> <!-- py -->
+            <div class="container-fluid row">
+                <div class="col-lg-6">
+                    <label for="inputIncome">Income</label>
+                    <input name="inputIncome" type="text" class="form-control" placeholder="Enter the Income"> <!-- I -->
+                    <label for="inputGoodX">Good X's Preference</label>
+                    <input name="inputGoodX" type="text" class="form-control" placeholder="Enter X's Preference"> <!-- Alpha -->
+                    <label for="inputGoodY">Good Y's Preference</label>
+                    <input name="inputGoodY" type="text" class="form-control" placeholder="Enter Y's Preference"> <!-- Beta -->
+                    <label for="inputPriceX">Price of X</label>
+                    <input name="inputPriceX" type="text" class="form-control" placeholder="Enter Price of X"> <!-- px -->
+                    <label for="inputPriceY">Price of Y</label>
+                    <input name="inputPriceY" type="text" class="form-control" placeholder="Enter Price of Y"> <!-- py -->
+                </div>
             </div>
             <br>
             <button type="submit" class="btn btn-primary">Submit</button>
--- web/Personal/w5/BenPost.php
+++ web/Personal/w5/BenPost.php
@@ -1,18 +1,33 @@
-<?php 
-    $income = htmlspecialchars($_POST["inputIncome"]);
-    $priceX = htmlspecialchars($_POST["inputPriceX"]);
-    $priceY = htmlspecialchars($_POST["inputPriceY"]);
-    $goodsXPreference = htmlspecialchars($_POST["inputGoodX"]);
-    $goodsYPreference = htmlspecialchars($_POST["inputGoodY"]);
-    
-    $x = ($income * $goodsXPreference) / ($goodsYPreference * 2 * $priceX);
-    $y = ($income) / (2 * $priceY);
+<!DOCTYPE html>
+<html class="no-js"> 
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Super Calculator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php 
+            $income = htmlspecialchars($_POST["inputIncome"]);
+            $priceX = htmlspecialchars($_POST["inputPriceX"]);
+            $priceY = htmlspecialchars($_POST["inputPriceY"]);
+            $goodsXPreference = htmlspecialchars($_POST["inputGoodX"]);
+            $goodsYPreference = htmlspecialchars($_POST["inputGoodY"]);
+            
+            $x = ($income * $goodsXPreference) / ($goodsYPreference * 2 * $priceX);
+            $y = ($income) / (2 * $priceY);
 
-    echo "Income:  $income <br><br>";
-    echo "Price of X: $priceX <br><br>";
-    echo "Price of Y: $priceY <br><br>";
-    echo "Preference of X goods: $goodsXPreference <br><br>";
-    echo "Preference of Y goods: $goodsYPreference <br><br>";
-    echo "Quantity of X's goods to be sold is: $x <br>";
-    echo "Quantity of Y's goods to be sold is: $y <br>";
-?>
+            echo "Income:  $income <br><br>";
+            echo "Price of X: $priceX <br><br>";
+            echo "Price of Y: $priceY <br><br>";
+            echo "Preference of X goods: $goodsXPreference <br><br>";
+            echo "Preference of Y goods: $goodsYPreference <br><br>";
+            echo "Quantity of X's goods to be sold is: $x <br>";
+            echo "Quantity of Y's goods to be sold is: $y <br>";
+        ?>
+    </body>
+</html>
