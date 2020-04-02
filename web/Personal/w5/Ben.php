bd1663f1dc6a7ab6b885bd0caabb421f22a65078
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-04-01 21:10:50 -0600

    Ben's App

--- web/Personal/w5/Ben.php
+++ web/Personal/w5/Ben.php
@@ -11,7 +11,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link rel='stylesheet' type='text/css' media='screen' href='Ben.css'>
     </head>
-    <body class="bg-8">
+    <body class="bg-7">
     <h1>Optimizing Supply</h1>
         <form action="BenPost.php" method="POST">
             <div class="container-fluid row squish-center3">
