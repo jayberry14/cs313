1e1ae5087c1a787541653bec9f31f8b66fff7cc8
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-04-02 20:34:31 -0600

    Ben's App

--- web/Personal/w5/graph.php
+++ web/Personal/w5/graph.php
@@ -10,6 +10,67 @@
 <html>
 <head> 
 <meta charset="UTF-8"> 
+<script>
+	window.onload = function () {
+	
+	var chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		title:{
+			text: "Dynamic Viscosity Vs Density over Temperature of Water"
+		},
+		axisX:{
+			title: "Temperature [°C]"
+		},
+		axisY:{
+			title: "Dynamic Viscosity [mPa.s]",
+			titleFontColor: "#4F81BC",
+			lineColor: "#4F81BC",
+			labelFontColor: "#4F81BC",
+			tickColor: "#4F81BC"
+		},
+		axisY2:{
+			title: "Density [g/cm³]",
+			titleFontColor: "#C0504E",
+			lineColor: "#C0504E",
+			labelFontColor: "#C0504E",
+			tickColor: "#C0504E",
+			includeZero: false
+		},
+		legend:{
+			cursor: "pointer",
+			dockInsidePlotArea: true,
+			itemclick: toggleDataSeries
+		},
+		data: [{
+			type: "line",
+			name: "Dynamic Viscosity",
+			markerSize: 0,
+			toolTipContent: "Temperature: {x} °C <br>{name}: {y} mPa.s",
+			showInLegend: true,
+			dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+		},{
+			type: "line",
+			axisYType: "secondary",
+			name: "Density",
+			markerSize: 0,
+			toolTipContent: "Temperature: {x} °C <br>{name}: {y} g/cm³",
+			showInLegend: true,
+			dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+		}]
+	});
+	chart.render();
+	
+	function toggleDataSeries(e){
+		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+			e.dataSeries.visible = false;
+		}
+		else{
+			e.dataSeries.visible = true;
+		}
+		chart.render();
+	}
+}
+</script>
 </head>
 <body>
 <div id="chartContainer" style="height: 370px; width: 100%;">
@@ -98,66 +159,5 @@
 	</table>
 </div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<script>
-	window.onload = function () {
-	
-	var chart = new CanvasJS.Chart("chartContainer", {
-		animationEnabled: true,
-		title:{
-			text: "Dynamic Viscosity Vs Density over Temperature of Water"
-		},
-		axisX:{
-			title: "Temperature [°C]"
-		},
-		axisY:{
-			title: "Dynamic Viscosity [mPa.s]",
-			titleFontColor: "#4F81BC",
-			lineColor: "#4F81BC",
-			labelFontColor: "#4F81BC",
-			tickColor: "#4F81BC"
-		},
-		axisY2:{
-			title: "Density [g/cm³]",
-			titleFontColor: "#C0504E",
-			lineColor: "#C0504E",
-			labelFontColor: "#C0504E",
-			tickColor: "#C0504E",
-			includeZero: false
-		},
-		legend:{
-			cursor: "pointer",
-			dockInsidePlotArea: true,
-			itemclick: toggleDataSeries
-		},
-		data: [{
-			type: "line",
-			name: "Dynamic Viscosity",
-			markerSize: 0,
-			toolTipContent: "Temperature: {x} °C <br>{name}: {y} mPa.s",
-			showInLegend: true,
-			dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
-		},{
-			type: "line",
-			axisYType: "secondary",
-			name: "Density",
-			markerSize: 0,
-			toolTipContent: "Temperature: {x} °C <br>{name}: {y} g/cm³",
-			showInLegend: true,
-			dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
-		}]
-	});
-	chart.render();
-	
-	function toggleDataSeries(e){
-		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-			e.dataSeries.visible = false;
-		}
-		else{
-			e.dataSeries.visible = true;
-		}
-		chart.render();
-	}
-}
-</script>
 </body>
 </html> 
