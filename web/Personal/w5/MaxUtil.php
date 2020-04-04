429ccf0e6796a553922ca1e5253c91f09cfda572
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-04-03 20:57:09 -0600

    Ben's App

--- web/Personal/w5/MaxUtil.php
+++ web/Personal/w5/MaxUtil.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js"> 
+<html class="bg-8"> 
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,7 +12,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link rel='stylesheet' type='text/css' media='screen' href='Ben.css'>
     </head>
-    <body class="bg-8">
+    <body>
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
